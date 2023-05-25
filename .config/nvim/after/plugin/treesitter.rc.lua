f3a21910d89ec0fddb16db320a2ce77485bbb869
nenoteerawat <nenoteerawat@gmail.com>
2023-05-25 17:00:58 +0700

    Update dotfile config

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -1,10 +1,7 @@
-if !exists('g:loaded_nvim_treesitter')
-  echom "Not loaded treesitter"
-  finish
-endif
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
 
-lua <<EOF
-require'nvim-treesitter.configs'.setup {
+ts.setup {
   highlight = {
     enable = true,
     disable = {},
@@ -14,21 +11,28 @@ require'nvim-treesitter.configs'.setup {
     disable = {},
   },
   ensure_installed = {
+    "markdown",
+    "markdown_inline",
     "tsx",
+    "typescript",
     "toml",
     "fish",
     "php",
     "json",
     "yaml",
     "swift",
+    "css",
     "html",
-    "scss"
+    "lua"
   },
   autotag = {
     enable = true,
+  },
+  context_commentstring = {
+    enable         = true,
+    enable_autocmd = false,
   }
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-EOF
