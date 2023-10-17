53ee9b88acd692c6172c61d559fd0b26b858d378
nenoteerawat <nenoteerawat@gmail.com>
2023-10-17 11:04:12 +0700

    Still use peco to find history command

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -11,9 +11,12 @@ end
 
 function fzf_change_directory
   begin
+    set ignore_dir '\.git|\.terragrunt-cache|node_modules'
     echo $HOME/.config
     find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
-    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v \.git
-    ls -ad $HOME/pttep/*/* |grep -v \.git
+    find $HOME/.ghq/gitlab.tools.pttep.com/digital-workspace-platform/delorean-infrastructure/iac/* -maxdepth 1 -type d|grep -v -E $ignore_dir
+    find $HOME/.ghq/gitlab.tools.pttep.com/digital-workspace-platform/delorean-platform/* -maxdepth 1 -type d|grep -v -E $ignore_dir
+    ls -ad */|perl -pe "s#^#$PWD/#"|grep -v -E $ignore_dir
+    ls -ad $HOME/pttep/*/* |grep -v -E $ignore_dir
   end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
