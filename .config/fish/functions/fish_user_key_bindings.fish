2e4b2e385f486fba7aae9bb354d83f50ddc7c9b0
Teerawat Yaemsai <nenoteerawat@gmail.com>
2023-12-22 11:01:46 +0700

    Clean old configuration

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
 function fish_user_key_bindings
 
   # peco
-  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+  # bind \cr peco_select_history # Bind for peco select history to Ctrl+R
 
   # fzf
   bind \cf fzf_change_directory
