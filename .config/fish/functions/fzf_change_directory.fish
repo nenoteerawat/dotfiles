f4a7fa7871398ebe6c63fa6552a8bfa2e475820f
Teerawat Yaemsai <nenoteerawat@gmail.com>
2024-08-27 22:36:14 +0700

    update dotfile

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -17,6 +17,9 @@ function fzf_change_directory
         find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
         find $HOME/.ghq/gitlab.tools.pttep.com/digital-workspace-platform/delorean-infrastructure/iac/* -maxdepth 1 -type d | grep -v -E $ignore_dir
         find $HOME/.ghq/gitlab.tools.pttep.com/digital-workspace-platform/delorean-platform/* -maxdepth 1 -type d | grep -v -E $ignore_dir
+        find $HOME/.ghq/gitlab.tools.pttep.com/ep-digital-platform/* -maxdepth 1 -type d | grep -v -E $ignore_dir
+        find $HOME/.ghq/gitlab.tools.pttep.com/devsecops/* -maxdepth 1 -type d | grep -v -E $ignore_dir
+        find $HOME/.ghq/gitlab.tools.pttep.com/centralized-pipeline/* -maxdepth 1 -type d | grep -v -E $ignore_dir
         ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v -E $ignore_dir
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
